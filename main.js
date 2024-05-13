6455e781838ee3b3c6c9af5cffa93bcf8e98e8db
DevBonatto <lucca.scaquetti@hotmail.com>
2024-05-12 23:29:25 -0300

    :boom: fix: array organization

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ const section2 = document.querySelector('.section-02')
 const section2P = document.querySelector('.section-02 p')
 const section2H1 = document.querySelector('.section-02 h1')
 
-const phrases = ['A vida trará coisas boas se tiver paciência.', 'Muitas das grandes realizações do mundo foram feitas por homens cansados e desanimados que continuaram o seu trabalho.', 'Você sempre será a sua melhor companhia!', 'Não compense na ira o que lhe falta na razão.', 'Aquele que se importa com o sentimento dos outros, não é um tolo.', 'A paciência na adversidade é sinal de um coração sensível.', 'Vencer é 90 por cento suor e 10 por cento de engenho.', 'A vontade das pessoas é a melhor das leis.', 'O verdadeiro homem mede a sua força, quando se defronta com o obstáculo.', 'Quem quer vencer um obstáculo deve armar-se da força do leão e da prudência da serpente.', 'Realize o óbvio, pense no improvável e conquiste o impossível.']
+const phrases = [
+  'A vida trará coisas boas se tiver paciência.',
+  'Muitas das grandes realizações do mundo foram feitas por homens cansados e desanimados que continuaram o seu trabalho.',
+  'Você sempre será a sua melhor companhia!',
+  'Não compense na ira o que lhe falta na razão.',
+  'Aquele que se importa com o sentimento dos outros, não é um tolo.',
+  'A paciência na adversidade é sinal de um coração sensível.',
+  'Vencer é 90 por cento suor e 10 por cento de engenho.',
+  'A vontade das pessoas é a melhor das leis.',
+  'O verdadeiro homem mede a sua força, quando se defronta com o obstáculo.',
+  'Quem quer vencer um obstáculo deve armar-se da força do leão e da prudência da serpente.',
+  'Realize o óbvio, pense no improvável e conquiste o impossível.'
+]
 
 let randomNumber = Number(Math.round(Math.random() * 10))
 let attempts = 0
